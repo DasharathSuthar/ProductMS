18d49bf4ecb2ede7f195a250cd26954c9c5bca99
Dasharath Suthar <suthardasharath4@gmail.com>
2025-06-19 23:01:13 +0530

    Added Dashboard component to display the main dashboard content.

---
+++ Frontend/src/Admin-Pages/components/dashboard/Dashboard.jsx
@@ -0,0 +1,17 @@
+
+
+const Dashboard = () => {
+    return (
+        <>
+            <div className='pb-6 text-xl uppercase text-black'>
+                <h1>Dashboard</h1>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                Test
+            </div>
+
+        </>
+    )
+}
+
+export default Dashboard
