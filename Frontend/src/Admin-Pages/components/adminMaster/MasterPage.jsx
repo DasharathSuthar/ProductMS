46d89f3cdf8e72fc439e6c38ddf0d027950a90fa
Dasharath Suthar <suthardasharath4@gmail.com>
2025-06-19 23:01:32 +0530

    Created MasterPage component to serve as the main layout for admin pages.

---
+++ Frontend/src/Admin-Pages/components/adminMaster/MasterPage.jsx
@@ -0,0 +1,19 @@
+import {Outlet} from 'react-router-dom'
+import Sidebar from '../sidebar/Sidebar'
+import Header from '../header/Header'
+
+const MasterPage = () => {
+    return (
+        <div className="flex ">
+            <Sidebar></Sidebar>
+            <div className="flex-1 bg-gray-200">
+                <Header></Header>
+                <div className="p-10 ">
+                    <Outlet></Outlet>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MasterPage
