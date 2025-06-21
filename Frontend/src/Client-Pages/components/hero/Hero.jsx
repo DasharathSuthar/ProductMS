181ca8f189eef6e8d9fc13362d01bd8366bdf667
Dasharath Suthar <suthardasharath4@gmail.com>
2025-06-21 23:07:58 +0530

    Remove unnecessary min-height from Hero component and clean up background text

--- Frontend/src/Client-Pages/components/hero/Hero.jsx
+++ Frontend/src/Client-Pages/components/hero/Hero.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Hero = ({ title, description }) => {
     return (
         <>
-            <div className='hero relative overflow-hidden min-h-screen bg-gradient-to-b from-[#0c0c1d] to-[#111132]'>
+            <div className='hero relative overflow-hidden  bg-gradient-to-b from-[#0c0c1d] to-[#111132]'>
                 <div className="wrapper max-w-[1000px] mx-auto h-full px-4 py-16">
                     <div className="flex flex-col md:flex-row h-full items-start  gap-10">
                         <div className="w-full md:w-1/2 flex flex-col justify-center gap-6">
@@ -29,9 +29,7 @@ const Hero = ({ title, description }) => {
                 </div>
 
                 {/* Background Text */}
-                <div className="absolute text-[20vh] sm:text-[30vh] md:text-[50vh] text-black opacity-10 whitespace-nowrap -bottom-12 md:-bottom-28 left-4 md:left-1/2 md:-translate-x-1/2">
-                    ProductMS
-                </div>
+               
             </div>
         </>
     );
